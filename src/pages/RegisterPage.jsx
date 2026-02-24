663967203f2927448527a6bc13df1746b0d6031f
Rio Prasetio <rioprst17@gmail.com>
2026-02-24 20:12:12 +0700

    add components, hooks, pages, and states

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import RegisterInput from '../components/RegisterInput';
+import { useDispatch } from 'react-redux';
+import { asyncRegisterUser } from '../states/users/action';
+
